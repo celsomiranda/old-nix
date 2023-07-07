aa2f8918be23edad83c880b36806b5e00a4fbf83
Celso Miranda <769237+celsomiranda@users.noreply.github.com>
2023-07-07 16:51:03 +0100

    fix typo

--- hosts/isctex/hardware-configuration.nix
+++ hosts/isctex/hardware-configuration.nix
@@ -25,5 +25,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = true;
-  powerManagement.cpuFreqGovernor = "performance"
+  powerManagement.cpuFreqGovernor = "performance";
 }
